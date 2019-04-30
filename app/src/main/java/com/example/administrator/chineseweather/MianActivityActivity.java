2cd1c574d55558c0dee552a440c2c5e12162f342
xmhgh <1533245514@qq.com>
2019-04-30 16:35:53 +0800

    First commit

---
+++ app/src/main/java/com/example/administrator/chineseweather/MianActivityActivity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.chineseweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MianActivityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mian_activity);
+    }
+}
